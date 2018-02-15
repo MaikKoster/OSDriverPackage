571e0975d89c66f2c15e2cbc0ca218cd26665335
Maik Koster <MaikKoster@users.noreply.github.com>
2018-02-15 17:36:52 -0600

    Add helper to get folder size

---
+++ OSDriverPackage/private/Get-FolderSize.ps1
@@ -0,0 +1,51 @@
+function Get-FolderSize {
+    <#
+    .SYNOPSIS
+        Returns the size of a the specified folder.
+
+    .DESCRIPTION
+        The Get-FolderSize CmdLet returns the size of the supplied folder.
+        It makes use of robocopy to speed up the evaluation of large folder sets.
+
+    #>
+    [CmdletBinding()]
+    [OutputType([PSCustomObject])]
+    param (
+        # Specifies the path of the folder.
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript({(Test-Path $_)})]
+        [string]$Path
+    )
+
+    process {
+        try {
+            Write-Verbose "Start getting folder size of '$Path'."
+
+            robocopy /l /nfl /ndl /njh $Path "$($Env:Temp)\VUVWXYZ" /e /bytes |
+                Where-Object { $_ -match "^[ \t]+(Dirs|Files|Bytes) :[ ]+\d" } |
+                ForEach-Object {
+                    if ($_ -like '*Dirs*'){
+                        $Dirs = ($_.Trim() -replace 'Dirs : ', '' -replace '[ ]{1,}',',').split(',')[1]
+                    } elseif ($_ -like '*Files*'){
+                        $Files = ($_.Trim() -replace 'Files : ', '' -replace '[ ]{1,}',',').split(',')[1]
+                    } elseif ($_ -like '*Bytes*') {
+                        $Bytes = ($_.Trim() -replace 'Bytes : ', '' -replace '[ ]{1,}',',').split(',')[1]
+                    }
+                }
+                Switch ($Bytes) {
+                    {$PSItem -le 1KB} {$Size = "$Bytes Bytes)"; Break}
+                    {$PSItem -le 1MB} {$Size = "$([Math]::Round($Bytes/1KB, 1)) KBytes)"; Break}
+                    {$PSItem -le 1GB} {$Size = "$([Math]::Round($Bytes/1MB,1)) MBytes)"; Break}
+                    default {$Size = "$([Math]::Round($Bytes/1GB,1)) GBytes)"; Break}
+                }
+                Write-Verbose "  Directories: $Dirs, Files: $Files, Size: $Size."
+                [PSCustomObject]@{
+                    Dirs = $Dirs
+                    Files = $Files
+                    Bytes = $Bytes
+                }
+        }catch{$null}
+        Write-Verbose "Finished getting folder size."
+    }
+}
