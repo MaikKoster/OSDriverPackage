78dfa805e9437e77ae8ae5c07f19dabf5bfd5014
Maik Koster <MaikKoster@users.noreply.github.com>
2018-02-15 17:35:58 -0600

    Add support to compress a Driver Package

---
+++ OSDriverPackage/public/Compress-OSDriverPackage.ps1
@@ -0,0 +1,59 @@
+function Compress-OSDriverPackage {
+    <#
+    .SYNOPSIS
+        Compresses the specified Driver Package into a cab file.
+
+    .DESCRIPTION
+        Compresses the specified Driver Package into a cab file.
+
+    .NOTES
+
+    #>
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        # Specifies the name and path of Driver Package that should be compressed.
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript({(Test-Path $_ -and (Get-Item $_).PSIsContainer)})]
+        [string]$Path,
+
+        # Specifies if an existing archive should be overwritten
+        [switch]$Force,
+
+        # Specifies if the original folder should be deleted after it has been compressed.
+        [switch]$RemoveFolder,
+
+        # Specifies, if the name and path of the compressed archive should be returned.
+        [switch]$Passthru
+    )
+
+    begin {
+        if (-not $PSBoundParameters.ContainsKey('Confirm')) {
+            $ConfirmPreference = $PSCmdlet.SessionState.PSVariable.GetValue('ConfirmPreference')
+        }
+        if (-not $PSBoundParameters.ContainsKey('WhatIf')) {
+            $WhatIfPreference = $PSCmdlet.SessionState.PSVariable.GetValue('WhatIfPreference')
+        }
+        Write-Verbose "Start compressing Driver Package."
+    }
+
+    process {
+        Write-Verbose " Compressing Driver Package '$Path'."
+
+        if ((Test-Path "$Path.cab") -and (-Not($Force.IsPresent))) {
+            throw "Archive '$Path.cab' exists already and '-Force' is not specified."
+        }
+
+        Compress-Folder -Path $Path -HighCompression -PassThru -Force -Verbose:$false
+
+        if ($RemoveFolder.IsPresent) {
+            if ($PSCmdlet.ShouldProcess("Removing folder '$Path'.")) {
+                $null = Remove-Item -Path $Path -Recurse -Force
+            }
+        }
+
+    }
+    end {
+        Write-Verbose "Finished compressing Driver Package."
+    }
+}
