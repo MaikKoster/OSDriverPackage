a0f08e7e8b66afac7619a17d380c1abc8ef6075b
Maik Koster <MaikKoster@users.noreply.github.com>
2018-10-29 17:55:39 -0500

    extend error handling for Compress-Folder

--- OSDriverPackage/private/Compress-Folder.ps1
+++ OSDriverPackage/private/Compress-Folder.ps1
@@ -11,20 +11,24 @@ function Compress-Folder {
     #>
     [CmdletBinding(SupportsShouldProcess)]
     param (
-        # Specifies the name and path of Folder that should be compress
+        # Specifies the name and path of Folder that should be compressed
         [Parameter(Mandatory, Position = 0, ValueFromPipeline, ValueFromPipelineByPropertyName)]
         [ValidateNotNullOrEmpty()]
+        [ValidateScript({Test-Path -Path $_})]
+        [Alias("DriverPath")]
         [Alias("FullName")]
-        [string[]]$Path,
+        [string]$Path,
 
-        # Specifies the name of the archive.
+        # Specifies the name and path of the archive.
         # On Default, the name of the folder will be used.
-        [string]$Name,
+        [Parameter(Position = 1, ValueFromPipelineByPropertyName)]
+        [Alias("DriverArchiveFile")]
+        [string]$Destination,
 
         # Specifies the type of archive.
-        # Possible values are CAB or ZIP
-        [ValidateSet('CAB', 'ZIP')]
-        [string]$ArchiveType = 'ZIP',
+        # Possible values are 'cab' or 'zip'
+        [ValidateSet('cab', 'zip')]
+        [string]$ArchiveType,
 
         # Specifies if High Compression should be used.
         [switch]$HighCompression,
@@ -45,85 +49,104 @@ function Compress-Folder {
     )
 
     process {
-        $script:Logger.Trace("Compress folder ('Path':'$Path', 'Name':'$Name', ArchiveType':'$ArchiveType', 'HighCompression':'$HighCompression', 'PassThru':'$PassThru', 'ShowMakeCabOutput':'$ShowMakeCabOutput', 'KeepMakeCabFile':'$KeepMakeCabFile', 'RemoveSource':'$RemoveSource')")
+        $script:Logger.Trace("Compress folder ('Path':'$Path', 'Destination':'$Destination', ArchiveType':'$ArchiveType', 'HighCompression':'$HighCompression', 'ShowMakeCabOutput':'$ShowMakeCabOutput', 'KeepMakeCabFile':'$KeepMakeCabFile', 'RemoveSource':'$RemoveSource')")
 
-        foreach ($Folder in $Path){
-            if ([string]::IsNullOrEmpty($Name)) {
-                $ArchiveBaseName = (Get-Item $Folder).Name
-            } else {
-                $ArchiveBaseName = $Name
+        if ([string]::IsNullOrWhiteSpace($ArchiveType)) {
+            if ([string]::IsNullOrWhitespace($Destination)) {
+                $Destination = "$((Get-Item -Path $Path).FullName.TrimEnd('\')).zip"
             }
-            $DestinationFolder = (Get-Item $Folder).Parent.FullName.TrimEnd('\')
-            $FolderFullName = (Get-Item $Folder).Fullname.TrimEnd('\')
 
-            if ($ArchiveType -eq 'ZIP') {
-                $ArchiveFileName = "$ArchiveBaseName.zip"
-                $ArchiveFullName = Join-Path -Path $DestinationFolder -ChildPath $ArchiveFileName
-                if ($PSCmdlet.ShouldProcess("Creating archive '$ArchiveFullName'.")) {
-                    $script:Logger.Debug("Compressing folder '$FolderFullName' to '$ArchiveFullName'.")
-                    if (Test-Path $ArchiveFullName) {
-                        Remove-Item -Path $ArchiveFullName -Force
-                    }
+            if ($Destination -like '*.cab') {
+                $ArchiveType = 'cab'
+            } else {
+                $ArchiveType = 'zip'
+            }
+
+        } else {
+            # ArchiveType takes precedence if specified
+            if ([string]::IsNullOrWhitespace($Destination)) {
+                $Destination = "$((Get-Item -Path $Path).FullName.TrimEnd('\')).$ArchiveType"
+            } else {
+                $Destionation = $Destination -replace '.zip|.cab', ".$ArchiveType"
+            }
+        }
+
+        # Files downloaded from the internet might be blocked.
+        # As this can cause very hard to diagnose issues especially with SCCM
+        # ensure that all files are unblocked before they are compressed.
+        Get-ChildItem $Path -Recurse | Unblock-File
+
+        if ($ArchiveType -eq 'zip') {
+            if ($PSCmdlet.ShouldProcess("Creating archive '$Destination'.")) {
+                $script:Logger.Debug("Compressing folder '$Path' to '$Destination'.")
+                if (Test-Path $Destination) {
+                    Remove-Item -Path $Destination -Force
+                }
+                try {
                     Add-Type -Assembly 'System.IO.Compression.Filesystem'
-                    [IO.Compression.ZIPFile]::CreateFromDirectory($FolderFullName, $ArchiveFullName)
+                    [IO.Compression.ZIPFile]::CreateFromDirectory($Path, $Destination)
+                } catch {
+                    $script:Logger.Error("Exception while calling 'Compress-Folder'")
+                    $script:Logger.Error("$($_.ToString())")
                 }
+            }
+        } else {
+            $ArchiveFileName = Split-Path -Path $Destination -Leaf
+            $ArchiveBasename = $ArchiveFileName -replace '.zip|.cab', ''
+            $DestinationFolder = Split-Path -Path $Destination -Parent
+            $script:Logger.Debug('Generating MakeCAB directive file.')
+            $DirectiveString = [System.Text.StringBuilder]::new()
+            [void]$DirectiveString.AppendLine(';*** MakeCAB Directive file;')
+            [void]$DirectiveString.AppendLine('.OPTION EXPLICIT')
+            [void]$DirectiveString.AppendLine(".Set CabinetNameTemplate=$ArchiveFileName")
+            [void]$DirectiveString.AppendLine(".Set DiskDirectory1=$DestinationFolder")
+            [void]$DirectiveString.AppendLine('.Set Cabinet=ON')
+            [void]$DirectiveString.AppendLine('.Set Compress=ON')
+            if ($HighCompression.IsPresent) {
+                [void]$DirectiveString.AppendLine('.Set CompressionType=LZX')
             } else {
-                $ArchiveFileName = "$ArchiveBaseName.cab"
-                $ArchiveFullName = Join-Path -Path $DestinationFolder -ChildPath $ArchiveFileName
-                $script:Logger.Debug('Generating MakeCAB directive file.')
-                $DirectiveString = [System.Text.StringBuilder]::new()
-                [void]$DirectiveString.AppendLine(';*** MakeCAB Directive file;')
-                [void]$DirectiveString.AppendLine('.OPTION EXPLICIT')
-                [void]$DirectiveString.AppendLine(".Set CabinetNameTemplate=$ArchiveFileName")
-                [void]$DirectiveString.AppendLine(".Set DiskDirectory1=$DestinationFolder")
-                [void]$DirectiveString.AppendLine('.Set Cabinet=ON')
-                [void]$DirectiveString.AppendLine('.Set Compress=ON')
-                if ($HighCompression.IsPresent) {
-                    [void]$DirectiveString.AppendLine('.Set CompressionType=LZX')
+                [void]$DirectiveString.AppendLine('.Set CompressionType=MSZIP')
+            }
+            [void]$DirectiveString.AppendLine('.Set CabinetFileCountThreshold=0')
+            [void]$DirectiveString.AppendLine('.Set FolderFileCountThreshold=0')
+            [void]$DirectiveString.AppendLine('.Set FolderSizeThreshold=0')
+            [void]$DirectiveString.AppendLine('.Set MaxCabinetSize=0')
+            [void]$DirectiveString.AppendLine('.Set MaxDiskFileCount=0')
+            [void]$DirectiveString.AppendLine('.Set MaxDiskSize=0')
+
+            # Files downloaded from the internet might be blocked.
+            # As this can cause very hard to diagnose issues especially with SCCM
+            # ensure that all files are unblocked when they are compressed.
+            Get-ChildItem $FolderFullName -Recurse | Unblock-File
+            $DirectivePath = Join-Path -Path $DestinationFolder -ChildPath "$ArchiveBaseName.ddf"
+            Get-ChildItem -Recurse $Folder | Where-Object { -Not($_.psiscontainer)} | Select-Object -ExpandProperty Fullname | Foreach-Object {
+                [void]$DirectiveString.AppendLine("""$_"" ""$($_.SubString($FolderFullName.Length + 1))""")
+            }
+            if ($PSCmdlet.ShouldProcess("Creating archive '$Destination'.")) {
+                $script:Logger.Debug("Compressing folder '$FolderFullName' to '$Destination'.")
+                $DirectiveString.ToString() | Out-File -FilePath $DirectivePath -Encoding UTF8
+                $script:Logger.Trace($DirectiveString.ToString())
+                if ($MakeCabOutput.IsPresent) {
+                    makecab /F $DirectivePath
                 } else {
-                    [void]$DirectiveString.AppendLine('.Set CompressionType=MSZIP')
+                    makecab /F $DirectivePath | Out-Null
                 }
-                [void]$DirectiveString.AppendLine('.Set CabinetFileCountThreshold=0')
-                [void]$DirectiveString.AppendLine('.Set FolderFileCountThreshold=0')
-                [void]$DirectiveString.AppendLine('.Set FolderSizeThreshold=0')
-                [void]$DirectiveString.AppendLine('.Set MaxCabinetSize=0')
-                [void]$DirectiveString.AppendLine('.Set MaxDiskFileCount=0')
-                [void]$DirectiveString.AppendLine('.Set MaxDiskSize=0')
-
-                # Files downloaded from the internet might be blocked.
-                # As this can cause very hard to diagnose issues especially with SCCM
-                # ensure that all files are unblocked when they are compressed.
-                Get-ChildItem $FolderFullName -Recurse | Unblock-File
-                $DirectivePath = Join-Path -Path $DestinationFolder -ChildPath "$ArchiveBaseName.ddf"
-                Get-ChildItem -Recurse $Folder | Where-Object { -Not($_.psiscontainer)} | Select-Object -ExpandProperty Fullname | Foreach-Object {
-                    [void]$DirectiveString.AppendLine("""$_"" ""$($_.SubString($FolderFullName.Length + 1))""")
-                }
-                if ($PSCmdlet.ShouldProcess("Creating archive '$ArchiveFullName'.")) {
-                    $script:Logger.Debug("Compressing folder '$FolderFullName' to '$ArchiveFullName'.")
-                    $DirectiveString.ToString() | Out-File -FilePath $DirectivePath -Encoding UTF8
-                    $script:Logger.Trace($DirectiveString.ToString())
-                    if ($MakeCabOutput.IsPresent) {
-                        makecab /F $DirectivePath
-                    } else {
-                        makecab /F $DirectivePath | Out-Null
-                    }
-                    if (-Not($KeepMakeCabFile.IsPresent)) {
-                        $script:Logger.Trace('Removing temporary files.')
-                        Remove-Item $DirectivePath
-                        if (Test-Path 'setup.inf') {Remove-Item 'setup.inf' -Force}
-                        if (Test-Path 'setup.rpt') {Remove-Item 'setup.rpt' -Force}
-                    }
+                if (-Not($KeepMakeCabFile.IsPresent)) {
+                    $script:Logger.Trace('Removing temporary files.')
+                    Remove-Item $DirectivePath
+                    if (Test-Path 'setup.inf') {Remove-Item 'setup.inf' -Force}
+                    if (Test-Path 'setup.rpt') {Remove-Item 'setup.rpt' -Force}
                 }
             }
+        }
 
-            if ($RemoveSource.IsPresent){
-                $script:Logger.Debug('Removing source files.')
-                Remove-Item -Path $Folder -Recurse -Force
-            }
+        if ($RemoveSource.IsPresent){
+            $script:Logger.Debug('Removing source files.')
+            Remove-Item -Path $Folder -Recurse -Force
+        }
 
-            if ($PassThru.IsPresent) {
-                $ArchiveFullName
-            }
+        if ($PassThru.IsPresent) {
+            $Destination
         }
     }
 }
